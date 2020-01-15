154c3bcd83f7e1ae36c9c9971a302c5e2b4219c3
tsubasa-0283 <tsubasaokamoto7@gmail.com>
2020-01-15 19:03:13 +0900

    automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,4 +58,38 @@ $('#new_message').on('submit', function(e){
   });
   return false;
 })
+  var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.main__box:last').data("message-id");
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          //追加するHTMLの入れ物を作る
+          var insertHTML = '';
+          //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          //メッセージが入ったHTMLに、入れ物ごと追加
+          $('.main').append(insertHTML);
+          $('.main').animate({ scrollTop: $('.main')[0].scrollHeight});
+          $("#new_message")[0].reset();
+          $(".form__submit").prop("disabled", false);
+        }
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
